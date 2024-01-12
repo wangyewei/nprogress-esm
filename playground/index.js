33128a044eacd4f82e30da854d8bb89a306ad434
WangYeWei <826036140@qq.com>
2024-01-12 17:03:11 +0800

    workflow: playground update

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,15 @@
 import Progress from "../dist/index.js"
 
 window.addEventListener('DOMContentLoaded', () => {
-  const start = document.getElementById('btn-start')
-  const done = document.getElementById('btn-done')
+  const start = document.getElementById('start')
+  const set = document.getElementById('set')
+  const done = document.getElementById('done')
   start.onclick = () => {
     Progress.start()
+  }
+
+  set.onclick = () => {
+    Progress.set(40)
   }
 
   done.onclick = () => {
