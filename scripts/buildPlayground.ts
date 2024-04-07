fc77ac9b6d857d02a3f8d798119d8abf5b7891ef
wangyewei <826036140@qq.com>
2024-04-07 11:32:46 +0800

    chore(scripts): sync playgrounds build

--- scripts/buildPlayground.ts
+++ scripts/buildPlayground.ts
@@ -2,7 +2,7 @@ import fg from 'fast-glob'
 import { copy } from 'fs-extra'
 
 fg.sync('dist/**').forEach(async (file) => {
-  Promise.all([copy(file, file.replace(/^dist\//, 'playground/dist/'))]).catch(
+  Promise.all([copy(file, file.replace(/^dist\//, 'playgrounds/html/dist/'))]).catch(
     (err) => {
       console.log(`Faild to running scripts of 'buildStyle': ${err}`)
     }
